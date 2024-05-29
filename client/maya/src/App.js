a5609d42cf6885a109383ff7ac4c771e2d18e10b
sookiemonster <sookiemonster216@gmail.com>
2024-05-29 15:38:06 -0400

    Add basic layout

--- client/maya/src/App.js
+++ client/maya/src/App.js
@@ -1,12 +1,16 @@
 import './App.scss';
 import Calendar from './components/Calendar.js'
+import NavBar from './components/NavBar.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
-      <Calendar/>
+      <div id="layout-container">
+        <NavBar />
+        <Calendar/>
+      </div>
     </div>
   );
 }
